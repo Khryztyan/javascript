9d1bd372e84b20106d72799d89ac772f7bd1f50e
cristian <petrone_137@hotmail.com>
2023-07-10 13:16:12 -0300

    modifico enlace de las fotos

--- Js/main.js
+++ Js/main.js
@@ -27,29 +27,29 @@ while(playerAlive() && nivel<10){
         energyPlayer =  energyPlayer - GUN;
         console.log("¡Te han disparado!");
         console.log("Ahora tu energía es de " + energyPlayer);
-        document.write('<div class="card"><img src="../img/gun.png"></div>')
+        document.write('<div class="card"><img src="img/gun.png"></div>')
     } else if(niveles == BOMB){
         energyPlayer =  energyPlayer - BOMB;
         console.log("¡Ha estallado una bomba!");
         console.log("Ahora tu energía es de " + energyPlayer);
-        document.write('<div class="card"><img src="../img/bomb.png"></div>') 
+        document.write('<div class="card"><img src="img/bomb.png"></div>') 
     }if(niveles == POWER){
         energyPlayer =  energyPlayer + POWER;
         console.log("¡Has conseguido un RedBull!");
         console.log("Ahora tu energía es de " + energyPlayer);
-        document.write('<div class="card"><img src="../img/power.png""></div>')
+        document.write('<div class="card"><img src="img/power.png""></div>')
     } else if(niveles == SAFETY){
         energyPlayer =  energyPlayer;
         console.log("¡Te has salvado esta vez!");
         console.log("Tu energía continua siendo " + energyPlayer);
-        document.write('<div class="card"><img src="../img/safety.png""></div>')
+        document.write('<div class="card"><img src="img/safety.png""></div>')
     }}
     console.log("---------------------")
 
     if(playerAlive()){
         console.log("¡Has ganado" + namePlayer + "!")
-        document.write('<div class="card"><img src="../img/youwin.jpg""></div>')
+        document.write('<div class="card"><img src="img/youwin.jpg""></div>')
     }else {
         console.log("¡Has perdido "+ namePlayer + "!")
-        document.write('<div class="card"><img src="../img/gameover.png""></div>')
+        document.write('<div class="card"><img src="img/gameover.png""></div>')
     }
